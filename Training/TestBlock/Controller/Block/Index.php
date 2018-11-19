31ea016de4e8b187a39f5c5d8e7608a57b63e139
vitvik <tkachukvitaliy@gmail.com>
2018-11-19 20:59:59 +0200

    task_3.3

--- Training/TestBlock/Controller/Block/Index.php
+++ Training/TestBlock/Controller/Block/Index.php
@@ -5,13 +5,15 @@ namespace Training\TestBlock\Controller\Block;
 class Index extends \Magento\Framework\App\Action\Action
 {
     private $layoutFactory;
+    private $resultRawFactory;
 
     public function __construct(
 
         \Magento\Framework\App\Action\Context $context,
-        \Magento\Framework\View\LayoutFactory $layoutFactory
-
+        \Magento\Framework\View\LayoutFactory $layoutFactory,
+        \Magento\Framework\Controller\Result\RawFactory $resultRawFactory
 )   {
+        $this->resultRawFactory = $resultRawFactory;
         $this->layoutFactory = $layoutFactory;
         parent::__construct($context);
     }
@@ -20,6 +22,9 @@ class Index extends \Magento\Framework\App\Action\Action
     {
        $layout = $this->layoutFactory->create();
        $block =  $layout->createBlock('Training\TestBlock\Block\Test');
-       $this->getResponse()->appendBody($block->toHtml());
+//       $this->getResponse()->appendBody($block->toHtml());
+        $resultRaw = $this->resultRawFactory->create();
+        $resultRaw->setContents($block->toHtml());
+        return $resultRaw;
     }
 }
